7ff7f41c5861710408cc27b16876516ab60a7f8c
Danut <deny40167@gmail.com>
2025-11-14 12:53:10 +0200

    css update

--- frontend/src/admin/HomePage.jsx
+++ frontend/src/admin/HomePage.jsx
@@ -5,7 +5,6 @@ export default function HomePage() {
   return (
     <section className="homepage">
       <div className="overlay">
-        <h1>VIGILENT SECURITY</h1>
       </div>
     </section>
   );
